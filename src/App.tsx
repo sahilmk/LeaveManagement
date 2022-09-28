34094bd2c9c5b2d73de173b07cd03fe02ae6bb4d
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-09-28 19:25:56 +0530

    #8 sidebar component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react"
-import { Sidebar } from "./stories"
 
 function App() {
   return (
     <div className="App">
-      <Sidebar user='sahil' position='developer' />
     </div>
   )
 }
