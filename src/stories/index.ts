a860abb4a5d7f1a34afd1a12bee2cab31b9dae91
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-09-29 15:56:27 +0530

    #8 Done the change in index file

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -2,13 +2,7 @@ import Input from './Input'
 import ButtonComponent from './ButtonComponent'
 import SidebarTab from './Sidebar tab'
 import PageTitle from './PageTitle'
-<<<<<<< HEAD
 import Sidebar from './Sidebar'
 import NavbarComponent from './NavbarComponent'
 
 export { Input, ButtonComponent, SidebarTab, PageTitle, Sidebar, NavbarComponent }
-=======
-import NavbarComponent from './NavbarComponent'
-
-export { Input, ButtonComponent, SidebarTab, PageTitle, NavbarComponent }
->>>>>>> 6-page-title
