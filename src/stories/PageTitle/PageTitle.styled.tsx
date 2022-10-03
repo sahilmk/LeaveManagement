f2893aead99c356bb463fd76f4375211c9f00bf9
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-10-03 14:36:18 +0530

    #6 Change px to rem and create the another file for the colors

--- src/stories/PageTitle/PageTitle.styled.tsx
+++ src/stories/PageTitle/PageTitle.styled.tsx
@@ -1,37 +1,38 @@
 import styled from "styled-components";
+import { Theme } from "../../Theme";
 
 export const PageTitleStyle = styled.div`
     justify-content: space-between;
     box-sizing: border-box;
     align-items: center;
     display: flex;
-    background-color: #fbfbfb;
-    width: calc(100% - 420px);
-    padding:  30px 30px 18px;
-    margin-left: 420px;
+    background-color: ${Theme.colors.lotion};
+    width: calc(100% - 42rem);
+    padding:  3rem 3rem 1.8rem;
+    margin-left: 42rem;
 
     .logindate{
         display: inline-block;
         font-family: SegoeUI;
-        color: #173346;
-        margin-bottom: 10px;
-        font-size: 19.5px;
+        color: ${Theme.colors.yankeesBlue};
+        margin-bottom: 1rem;
+        font-size: 1.95rem;
     }
 
     .nameofpage{
         font-family: SegoeUI;
-        color: #173346;
+        color: ${Theme.colors.yankeesBlue};
         font-weight: 600;
-        font-size: 30px;
+        font-size: 3rem;
     }
 
     .childpage{
         font-family: SegoeUI;
-        color: #173346;
-        font-size: 24px;
+        color: ${Theme.colors.yankeesBlue};
+        font-size: 2.4rem;
     }
 
     button{
-        height: 50px;
+        height: 5rem;
     }
 `
