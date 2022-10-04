dbce759894e82de11f4ddabf4bc3dd4d03c5eb25
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-10-04 10:47:15 +0530

    #8 Try to make the sidebar more dynamic and reusable according to the review.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { Sidebar } from "./stories"
+import { sidebarTabData } from './Util/Constants'
 
 function App() {
   return (
     <div className="App">
-      <Sidebar user='sahil' position='dev' />
+      <Sidebar user='sahil' position='dev' sidebarTabData={sidebarTabData} userImage='./assets/images/profile.png' />
     </div>
   )
 }
