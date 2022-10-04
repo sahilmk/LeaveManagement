b46295f9792b7fe440001f95b6b86cca4582a1f5
Pinak Bhuva <pinak.bhuva@smartsensesolutions.com>
2022-10-04 18:14:22 +0530

    #13 profile page updated as per requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from "react"
-import { Dashboard } from "./Pages"
+import { Dashboard } from "./Pages";
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Dashboard/Dashboard.page.tsx
+++ src/Pages/Dashboard/Dashboard.page.tsx
@@ -1,28 +1,42 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { NavbarComponent, Sidebar } from '../../stories'
+import { NavbarComponent, Sidebar } from "../../stories";
 import { LeaveRequest } from "../../components";
-import * as styles from './Dashboard.module.scss'
+import { sidebarTabData } from "../../Util/Constants";
+import * as styles from "./Dashboard.module.scss";
 
 function Dashboard() {
-    return (
-        <>
-            <NavbarComponent />
-            <Sidebar user='John Doe' position='Admin' />
-            <div className={styles.maincontent}>
-                <Routes>
-                    <Route path="/home" element={<h1>Home</h1>} />
-                    <Route path="/holidays" element={<h1>Holidays</h1>} />
-                    <Route path="leaverequest" element={<LeaveRequest />} />
-                    <Route path="/manageleaverequest" element={<h1>manageleaverequest</h1>} />
-                    <Route path="/employeelist" element={<h1>employeelist</h1>} />
-                    <Route path="/employeeleaveslist" element={<h1>employeeleaveslist</h1>} />
-                    <Route path="/leavetype" element={<h1>leavetype</h1>} />
-                    <Route path="/department" element={<h1>department</h1>} />
-                </Routes>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <NavbarComponent />
+      <Sidebar
+        user="John Doe"
+        position="Admin"
+        sidebarTabData={sidebarTabData}
+        userImage={"./assets/images/profile.png"}
+      />
+      <div className={styles.maincontent}>
+        <Routes>
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/holidays" element={<h1>Holidays</h1>} />
+          <Route
+            path="leaverequest"
+            element={<LeaveRequest logindate={"18/10/2022"} />}
+          />
+          <Route
+            path="/manageleaverequest"
+            element={<h1>manageleaverequest</h1>}
+          />
+          <Route path="/employeelist" element={<h1>employeelist</h1>} />
+          <Route
+            path="/employeeleaveslist"
+            element={<h1>employeeleaveslist</h1>}
+          />
+          <Route path="/leavetype" element={<h1>leavetype</h1>} />
+          <Route path="/department" element={<h1>department</h1>} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
