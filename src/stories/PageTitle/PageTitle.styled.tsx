a960b516f4c147b9a5e1718a8bc1cc8913c09384
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-10-04 11:05:33 +0530

    #6 Try to make the page title component more dynamic as per the reviews.

--- src/stories/PageTitle/PageTitle.styled.tsx
+++ src/stories/PageTitle/PageTitle.styled.tsx
@@ -2,33 +2,31 @@ import styled from "styled-components";
 import { Theme } from "../../Theme";
 
 export const PageTitleStyle = styled.div`
+    background-color: ${Theme.colors.lotion};
     justify-content: space-between;
     box-sizing: border-box;
     align-items: center;
     display: flex;
-    background-color: ${Theme.colors.lotion};
-    width: calc(100% - 42rem);
     padding:  3rem 3rem 1.8rem;
-    margin-left: 42rem;
 
     .logindate{
-        display: inline-block;
-        font-family: SegoeUI;
         color: ${Theme.colors.yankeesBlue};
+        font-family: SegoeUI;
+        display: inline-block;
         margin-bottom: 1rem;
         font-size: 1.95rem;
     }
 
     .nameofpage{
-        font-family: SegoeUI;
         color: ${Theme.colors.yankeesBlue};
+        font-family: SegoeUI;
         font-weight: 600;
         font-size: 3rem;
     }
 
     .childpage{
-        font-family: SegoeUI;
         color: ${Theme.colors.yankeesBlue};
+        font-family: SegoeUI;
         font-size: 2.4rem;
     }
 
