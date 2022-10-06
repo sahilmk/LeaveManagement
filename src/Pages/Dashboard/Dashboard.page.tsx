bfc495873dbfe608bf7ec8df7dfd0ff364feba50
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-10-06 16:58:09 +0530

    #9 Converted static route path into constat

--- src/Pages/Dashboard/Dashboard.page.tsx
+++ src/Pages/Dashboard/Dashboard.page.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { NavbarComponent, Sidebar } from '../../stories'
-import * as styles from './Dashboard.module.scss'
+import { departmentPath, employeeleaveslistPath, employeelistPath, holidayPath, homePath, leavetypePath, manageleaverequestPath, sidebarTabData } from '../../Util/Constants'
+import styles from './Dashboard.module.scss'
 
 function Dashboard() {
     return (
         <>
             <NavbarComponent />
-            <Sidebar user='John Doe' position='Admin' />
+            <Sidebar user='John Doe' position='Admin' sidebarTabData={sidebarTabData} userImage={""} />
             <div className={styles.maincontent}>
                 <Routes>
-                    <Route path="/home" element={<h1>Home</h1>} />
-                    <Route path="/holidays" element={<h1>Holidays</h1>} />
-                    <Route path="/manageleaverequest" element={<h1>manageleaverequest</h1>} />
-                    <Route path="/employeelist" element={<h1>employeelist</h1>} />
-                    <Route path="/employeeleaveslist" element={<h1>employeeleaveslist</h1>} />
-                    <Route path="/leavetype" element={<h1>leavetype</h1>} />
-                    <Route path="/department" element={<h1>department</h1>} />
+                    <Route path={homePath} element={<h1>Home</h1>} />
+                    <Route path={holidayPath} element={<h1>Holidays</h1>} />
+                    <Route path={manageleaverequestPath} element={<h1>manageleaverequest</h1>} />
+                    <Route path={employeelistPath} element={<h1>employeelist</h1>} />
+                    <Route path={employeeleaveslistPath} element={<h1>employeeleaveslist</h1>} />
+                    <Route path={leavetypePath} element={<h1>leavetype</h1>} />
+                    <Route path={departmentPath} element={<h1>department</h1>} />
                 </Routes>
             </div>
         </>
