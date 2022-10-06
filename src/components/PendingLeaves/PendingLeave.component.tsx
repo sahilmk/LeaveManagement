452ed57a6a58f1eb7baebc6c24d16adaf8491182
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-10-06 10:22:50 +0530

    #14 Done some changes in styling of Pending Leave component

--- src/components/PendingLeaves/PendingLeave.component.tsx
+++ src/components/PendingLeaves/PendingLeave.component.tsx
@@ -40,137 +40,135 @@ function PendingLeave({ logindate }: pendingLeavePropType) {
             <PageTitle logindate={logindate} pagename={'Leaves'} innerPageNames={['Pending Leaves']} isinnerPage={true} isButton={false} />
 
             {/* Form to take the input for the filtering the table row. */}
-            <div className={style.py3} >
-                <div className={style.approvedpage}>
-                    <Form
-                        onSubmit={onSubmit}
-                        validate={validate}
-                        initialValues={{ type: 'Paid' }}
-                        render={({ handleSubmit }) => (
-                            <form onSubmit={handleSubmit}>
-                                <div className={style.displayflex}>
-                                    <div className={style.input}>
-                                        <Field name="startdate">
-                                            {(e) => (
-                                                <div>
-                                                    <label htmlFor="startdate">Start Date</label>
-                                                    <Input
-                                                        id='startdate'
-                                                        type='date'
-                                                        placeholder='Select Date'
-                                                        inputtype=''
-                                                        padding={'14px 18px 14px 19px'}
-                                                        width={300}
-                                                        onChange={e.input.onChange}
-                                                        onBlur={e.input.onBlur}
-                                                        onFocus={e.input.onFocus}
-                                                    />
-                                                    {e.meta.error && e.meta.touched && <span className={style.error}>{e.meta.error}</span>}
-                                                </div>
-                                            )}
-                                        </Field>
-                                    </div>
-
-                                    <div className={style.input}>
-                                        <Field name="enddate">
-                                            {(e) => (
-                                                <div>
-                                                    <label htmlFor="enddate">End Date</label>
-                                                    <Input
-                                                        id='enddate'
-                                                        type='date'
-                                                        placeholder='Select Date'
-                                                        inputtype=''
-                                                        padding={'14px 18px 14px 19px'}
-                                                        width={300}
-                                                        onChange={e.input.onChange}
-                                                        onBlur={e.input.onBlur}
-                                                        onFocus={e.input.onFocus} />
-                                                    {e.meta.error && e.meta.touched && <span className={style.error}>{e.meta.error}</span>}
-                                                </div>
-                                            )}
-                                        </Field>
-                                    </div>
-
-                                    <div className={style.input}>
-                                        <Field name="search">
-                                            {(e) => (
-                                                <div>
-                                                    <label htmlFor="search">Search</label>
-                                                    <Input
-                                                        id='search'
-                                                        type='text'
-                                                        placeholder='Select Date'
-                                                        inputtype=''
-                                                        padding={'14px 18px 14px 19px'}
-                                                        width={300}
-                                                        onChange={e.input.onChange}
-                                                        onBlur={e.input.onBlur}
-                                                        onFocus={e.input.onFocus} />
-                                                    {e.meta.error && e.meta.touched && <span className={style.error}>{e.meta.error}</span>}
-                                                </div>
-                                            )}
-                                        </Field>
-                                    </div>
+            <div className={style.approvedpage}>
+                <Form
+                    onSubmit={onSubmit}
+                    validate={validate}
+                    initialValues={{ type: 'Paid' }}
+                    render={({ handleSubmit }) => (
+                        <form onSubmit={handleSubmit}>
+                            <div className={style.displayflex}>
+                                <div className={style.input}>
+                                    <Field name="startdate">
+                                        {(e) => (
+                                            <div>
+                                                <label htmlFor="startdate">Start Date</label>
+                                                <Input
+                                                    id='startdate'
+                                                    type='date'
+                                                    placeholder='Select Date'
+                                                    inputtype=''
+                                                    padding={'14px 18px 14px 19px'}
+                                                    width={300}
+                                                    onChange={e.input.onChange}
+                                                    onBlur={e.input.onBlur}
+                                                    onFocus={e.input.onFocus}
+                                                />
+                                                {e.meta.error && e.meta.touched && <span className={style.error}>{e.meta.error}</span>}
+                                            </div>
+                                        )}
+                                    </Field>
                                 </div>
-                            </form>
-                        )}
-                    />
 
-                    {/* Table */}
-                    <DataTable width={'100%'}
-                        columns={[
-                            { field: "type", headerName: "Type", flex: 1 },
-                            { field: "reason", headerName: "Reason", flex: 1 },
-                            { field: "date", headerName: "Date", flex: 1 },
-                            { field: "appliedOn", headerName: "Applied On", flex: 1 },
-                            {
-                                field: "actions",
-                                headerName: "Action",
-                                sortable: false,
-                                renderCell: (params) => {
-                                    return (
-                                        <div>
-                                            <i
-                                                onClick={() => {
-                                                    console.log("eye");
-                                                }}
-                                                style={{ cursor: "pointer" }}
-                                                className="zmdi zmdi-eye"
-                                            >
-                                            </i>
-                                            <i
-                                                onClick={() => {
-                                                    console.log("eye");
-                                                }}
-                                                style={{ cursor: "pointer" }}
-                                                className="zmdi zmdi-close"
-                                            >
-                                            </i>
-                                        </div>
-                                    );
-                                },
-                                flex: 1,
+                                <div className={style.input}>
+                                    <Field name="enddate">
+                                        {(e) => (
+                                            <div>
+                                                <label htmlFor="enddate">End Date</label>
+                                                <Input
+                                                    id='enddate'
+                                                    type='date'
+                                                    placeholder='Select Date'
+                                                    inputtype=''
+                                                    padding={'14px 18px 14px 19px'}
+                                                    width={300}
+                                                    onChange={e.input.onChange}
+                                                    onBlur={e.input.onBlur}
+                                                    onFocus={e.input.onFocus} />
+                                                {e.meta.error && e.meta.touched && <span className={style.error}>{e.meta.error}</span>}
+                                            </div>
+                                        )}
+                                    </Field>
+                                </div>
+
+                                <div className={style.input}>
+                                    <Field name="search">
+                                        {(e) => (
+                                            <div>
+                                                <label htmlFor="search">Search</label>
+                                                <Input
+                                                    id='search'
+                                                    type='text'
+                                                    placeholder='Select Date'
+                                                    inputtype=''
+                                                    padding={'14px 18px 14px 19px'}
+                                                    width={300}
+                                                    onChange={e.input.onChange}
+                                                    onBlur={e.input.onBlur}
+                                                    onFocus={e.input.onFocus} />
+                                                {e.meta.error && e.meta.touched && <span className={style.error}>{e.meta.error}</span>}
+                                            </div>
+                                        )}
+                                    </Field>
+                                </div>
+                            </div>
+                        </form>
+                    )}
+                />
+
+                {/* Table */}
+                <DataTable width={'100%'}
+                    columns={[
+                        { field: "type", headerName: "Type", flex: 1 },
+                        { field: "reason", headerName: "Reason", flex: 1 },
+                        { field: "date", headerName: "Date", flex: 1 },
+                        { field: "appliedOn", headerName: "Applied On", flex: 1 },
+                        {
+                            field: "actions",
+                            headerName: "Action",
+                            sortable: false,
+                            renderCell: (params) => {
+                                return (
+                                    <div>
+                                        <i
+                                            onClick={() => {
+                                                console.log("eye");
+                                            }}
+                                            style={{ cursor: "pointer" }}
+                                            className="zmdi zmdi-eye"
+                                        >
+                                        </i>
+                                        <i
+                                            onClick={() => {
+                                                console.log("eye");
+                                            }}
+                                            style={{ cursor: "pointer" }}
+                                            className="zmdi zmdi-close"
+                                        >
+                                        </i>
+                                    </div>
+                                );
                             },
-                        ]}
-                        rows={[
-                            {
-                                id: "1",
-                                type: "Paid",
-                                reason: "Seek Leave",
-                                date: "12/01/2018 to 14/01/2018",
-                                appliedOn: "25/12/2017",
-                            },
-                            {
-                                id: "2",
-                                type: "Paid",
-                                reason: "Seek Leave",
-                                date: "10/01/2018",
-                                appliedOn: "25/12/2017",
-                            }
-                        ]}
-                    />
-                </div>
+                            flex: 1,
+                        },
+                    ]}
+                    rows={[
+                        {
+                            id: "1",
+                            type: "Paid",
+                            reason: "Seek Leave",
+                            date: "12/01/2018 to 14/01/2018",
+                            appliedOn: "25/12/2017",
+                        },
+                        {
+                            id: "2",
+                            type: "Paid",
+                            reason: "Seek Leave",
+                            date: "10/01/2018",
+                            appliedOn: "25/12/2017",
+                        }
+                    ]}
+                />
             </div>
         </>
     )
