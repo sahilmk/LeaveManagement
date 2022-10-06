1db73b725e4fd8618704286173c92aa4963be8f1
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-10-06 11:55:36 +0530

    #15 Rejected leave completed

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import LeaveRequest from "./LeaveRequest";
 import ApprovedLeave from "./ApprovedLeaves";
 import PendingLeave from "./PendingLeaves";
 import RejectedLeave from "./RejectedLeaves";
+import CancelledLeave from "./CancelledLeaves";
 
-export { LeaveRequest, ApprovedLeave, PendingLeave, RejectedLeave };
+export { LeaveRequest, ApprovedLeave, PendingLeave, RejectedLeave, CancelledLeave };
